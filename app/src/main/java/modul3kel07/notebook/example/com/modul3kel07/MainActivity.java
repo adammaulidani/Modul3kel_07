d329748bd36676898ef134bbf266a0c93e2eae22
adammaulidani <adammaulidani@gmail.com>
2018-11-06 11:54:55 +0700

    First Commit

---
+++ app/src/main/java/modul3kel07/notebook/example/com/modul3kel07/MainActivity.java
@@ -0,0 +1,30 @@
+package modul3kel07.notebook.example.com.modul3kel07;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import modul3kel07.notebook.example.com.modul3kel07.ui.home.HomeActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PrefManager prefManager = new PrefManager(getApplicationContext());
+                prefManager.setFirstTimeLaunch(true);
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/modul3kel07/notebook/example/com/modul3kel07/adapter/car/CarListener.java
@@ -0,0 +1,7 @@
+package modul3kel07.notebook.example.com.modul3kel07.adapter.car;
+
+import modul3kel07.notebook.example.com.modul3kel07.data.model.DataCar;
+
+public interface CarListener {
+    void onCarClick(DataCar dataCar);
+}
